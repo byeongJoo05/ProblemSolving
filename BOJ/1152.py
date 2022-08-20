2489797e91d168a9f4d2c5e16578e448ad8bc1c5
SBJ <thsqudwn05@gmail.com>
2022-08-20 23:17:38 +0900

    문자열

--- BOJ/1152.py
+++ BOJ/1152.py
@@ -1,11 +1,2 @@
-a, b = map(list,input().split())
-
-a.reverse()
-a_rev = ''.join(a)
-b.reverse()
-b_rev = ''.join(b)
-
-if int(a_rev) > int(b_rev):
-    print(a_rev)
-else:
-    print(b_rev)
+S = input().split()
+print(len(S))

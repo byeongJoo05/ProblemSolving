3f88a5174cad1cdb5c35de26c859c984ae052518
SonByeongJoo <thsqudwn05@gmail.com>
2022-11-28 18:51:25 +0900

    코딩테스트

--- BOJ/1018.py
+++ BOJ/1018.py
@@ -1,25 +1,27 @@
 n, m = map(int, input().split())
 
 board = []
-
+count = []
 for _ in range(n):
-    board.append(list(input()))
+    board.append(input())
 
-count = 0
-for i in range(n-1):
-    for j in range(m-1):
-        if board[i][j] != board[i+1][j+1]:
-            board[i+1][j+1] = board[i][j]
-            count +=1
-        if board[i][j] == board[i][j+1]:
-            if board[i][j] == "B":
-                board[i][j+1] = "W"
-                count +=1
-            elif board[i][j] == "W":
-                board[i][j+1] = "B"
-                count+=1
-        if board[i][j+1] != board[i+1][j]:
-            board[i+1][j] = board[i][j+1]
-            count+=1
+for i in range(n-7):
+    for j in range(m-7):
+        countW = 0
+        countB = 0
+        for a in range(i, i+8):
+            for b in range(j, j+8):
+                if (a+b) %2 == 0: # 짝수일 때
+                    if board[a][b] != 'B':
+                        countB +=1
+                    if board[a][b] != 'W':
+                        countW +=1
+                else:
+                    if board[a][b] !='W':
+                        countB +=1
+                    if board[a][b] !='B':
+                        countW +=1
+        count.append(countB)
+        count.append(countW)
 
-print(count)
+print(min(count))

48b4039511582c8b631197ac86810f23bf75a831
SonByeongJoo <thsqudwn05@gmail.com>
2023-05-23 22:33:07 +0900

    코딩테스트

--- programmers/예상 대진표.py
+++ programmers/예상 대진표.py
@@ -1,9 +1,12 @@
-def solution(n,a,b):
-    cnt=0
-    while True:
-        a = (a//2)+(a%2)
-        b = (b//2)+(b%2)
-        cnt+=1
-        if a==b:
+def solution(n, a, b):
+    answer = 0
+
+    while a != b:
+        answer += 1
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+
+        if a == b:
             break
-    return cnt
+
+    return answer

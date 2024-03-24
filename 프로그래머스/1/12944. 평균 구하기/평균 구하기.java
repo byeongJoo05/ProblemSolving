1dc3646b0f88a0044a7f4f783a453b7d5ebed14b
손병주 <thsqudwn05@gmail.com>
2024-03-24 17:08:48 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for (int i: arr) {
+            answer += (double) i;
+        }
+        
+        answer /= (double) arr.length;
+        return answer;
+    }
+}

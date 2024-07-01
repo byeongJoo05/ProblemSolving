1c046dede787a6c79b8eed93bf51215df2a2024f
손병주 <thsqudwn05@gmail.com>
2024-07-01 23:02:03 +0900

    [level 0] Title: 0 떼기, Time: 10.56 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        int digit = Integer.parseInt(n_str);
+        
+        String answer = digit + "";
+        return answer;
+    }
+}

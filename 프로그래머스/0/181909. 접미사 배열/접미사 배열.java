a37b47d104055bf53c3a57ec98c11fe2723573e0
손병주 <thsqudwn05@gmail.com>
2024-08-17 22:07:41 +0900

    [level 0] Title: 접미사 배열, Time: 1.04 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public List<String> solution(String my_string) {
+        List<String> answer = new ArrayList<>();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            answer.add(my_string.substring(i));
+        }
+        
+        answer.sort(Comparator.naturalOrder());
+        return answer;
+    }
+}
